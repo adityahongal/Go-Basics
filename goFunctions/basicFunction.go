522512c9a75f36c8b181acf8295f055935dd5b69
adityahongal <adityahongalwork@gmail.com>
2024-06-12 21:25:53 +0530

    Basic Function in Go

---
+++ goFunctions/basicFunction.go
@@ -0,0 +1,31 @@
+// Functions in Go
+
+// For example, the following function:
+
+// func add(x int, y int) int {
+//   return x+y
+// }
+
+// Accepts two integer parameters and returns another integer.
+
+// Here, func sub(x int, y int) int is known as the "function signature".
+
+package main
+
+import "fmt"
+
+func concat(s1 string, s2 string) string {
+	return s1 + s2
+}
+
+// don't touch below this line
+
+func main() {
+	test("Lane,", " happy birthday!")
+	test("Elon,", " hope that Tesla thing works out")
+	test("Go", " is fantastic")
+}
+
+func test(s1 string, s2 string) {
+	fmt.Println(concat(s1, s2))
+}
